f33e70b40bf51e5f2ec80f450e3bed3d11129f92
pratyushvatsalshukla <pratyushshukla9333@gmail.com>
2021-07-07 00:45:49 +0530

    Added Currency Piping, customized it to INR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,16 @@ export class AppComponent {
   title = 'pipes-in-use';
   name :string= ''; 
   dateInput:string='' ;
+  amountInput:string = '' ;
 
   onNameChange(value:any){
     this.name = value.target.value;
   }
   onDateChange(value:any){
     this.dateInput = value.target.value ;
+  }
+  onAmountChange(value:any){
+    this.amountInput = value.target.value ;
+
   }
 }
